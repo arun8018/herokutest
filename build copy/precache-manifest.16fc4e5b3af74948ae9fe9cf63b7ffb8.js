e5ee93687a55202deb69a64aab81598a4e54ff81
Arun KB <arun.babu@litmus7.com>
2020-04-12 15:33:40 +0530

    third commit

---
+++ build copy/precache-manifest.16fc4e5b3af74948ae9fe9cf63b7ffb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b741e95e5fd6e060f30221bdbe890315",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9095eb81743765c91776",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "61949e62e9319a6fa5ec",
+    "url": "/static/js/2.05a727bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.05a727bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9095eb81743765c91776",
+    "url": "/static/js/main.b5e52703.chunk.js"
+  },
+  {
+    "revision": "054da5feca992902f61b",
+    "url": "/static/js/runtime-main.d98a51f5.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
